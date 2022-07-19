4f8620063b7b743c009b77c1d354a7b24b5fcf81
Leonardo Pacheco <pachecoleonardo10@gmail.com>
2022-07-19 09:02:38 -0500

    Finish homeuser

--- src/components/homeUser/BGHomeUser.js
+++ src/components/homeUser/BGHomeUser.js
@@ -5,7 +5,11 @@ const BGHomeUser = () => {
   return (
     <div className='bg-home-user w-100' style={{backgroundImage: "url("+banner+")"}}>
         <div className="bg-home-diagonal"></div>
-        <div className="bg-home-content"></div>
+        <div className="bg-home-content">
+            <h2>Trust</h2>
+            <h2>Selflessness</h2>
+            <h2>Transparency</h2>
+        </div>
     </div>
   )
 }
--- src/pages/layout/LayoutOwn.js
+++ src/pages/layout/LayoutOwn.js
@@ -4,26 +4,35 @@ import { Layout } from "antd";
 import HomeUser from "../homeUser/HomeUser";
 import { useMoralis } from "react-moralis";
 import DarkButton from "../../components/global/DarkButton";
-import "./LayoutOwn.css"
+import "./LayoutOwn.css";
 
 const { Header, Sider, Content } = Layout;
 const LayoutOwn = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const { logout } = useMoralis();
   return (
-    <Layout >
-      <Sider className="sider" onCollapse={(val) => setIsCollapsed(val)} collapsed={isCollapsed} collapsible={true} width="17.5vw">Sider</Sider>
+    <Layout>
+      <Sider
+        className="sider"
+        onCollapse={(val) => setIsCollapsed(val)}
+        collapsed={isCollapsed}
+        collapsible={true}
+        width="17.5vw"
+      >
+        Sider
+      </Sider>
       <Layout>
         <Header className="header d-flex">
           Header
           <DarkButton onClick={logout}>Logout</DarkButton>
-
         </Header>
         <Content>
           <Router>
-            <Routes>
-              <Route path="/" element={<HomeUser />} />
-            </Routes>
+            <div className="max-height">
+              <Routes>
+                <Route path="/" element={<HomeUser />} />
+              </Routes>
+            </div>
           </Router>
         </Content>
       </Layout>

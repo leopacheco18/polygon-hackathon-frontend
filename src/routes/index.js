1c3ae8e683983ef6b2e5c8eda82e78e4628caa11
Leonardo Pacheco Asus <leo@tobydev.com>
2022-07-20 20:25:40 -0500

    Fix routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,17 +5,15 @@ import Foundations from "../pages/foundations/Foundations";
 /* Importing the icons from the react-icons library. */
 import {
     AiFillHome,
-    AiOutlineFileAdd,
     AiOutlineFontColors,
     AiOutlineToTop,
 } from "react-icons/ai";
 
 /* Creating an array of objects. Each object has a path, title, component, and icon. */
 const authProtectedRoutes = [
-    { path: '/', title: 'Home', component: <Foundations />, icon: <AiFillHome />, exact: true },
+    { path: '/', title: 'Home', component: <HomeUser />, icon: <AiFillHome />, exact: true },
     { path: '/foundations', title: 'Foundations', component: <Foundations />, icon: <AiOutlineFontColors /> },
-    { path: '/top-contributors', title: 'Top Contributors', component: <HomeUser />, icon: <AiOutlineToTop /> },
-    { path: '/register-foundation', title: 'Register your foundation', component: <HomeUser />, icon: <AiOutlineFileAdd /> }
+    { path: '/top-contributors', title: 'Top Contributors', component: <></>, icon: <AiOutlineToTop /> }
 ]
 
 /* Exporting the array of objects. */
